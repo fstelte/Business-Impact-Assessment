0723e79ebcf7506f19e39b5cb5197a83190491c2
Mick Hilhorst <m.hilhorst@goose-it.nl>
2023-06-06 19:18:20 +0200

    Fix for Env / Visual changes

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # config.py
 import os
+from dotenv import load_dotenv
 
 class Config(object):
     DEBUG = False
@@ -7,9 +8,12 @@ class Config(object):
 
 class ConfigDevelopment(Config):
 
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    #if(SECRET_KEY is None):
-    #    raise ValueError("No SECRET_KEY set please follow the README")
+    # Load environment variables from .env
+    load_dotenv()
+    if 'SECRET_KEY' in os.environ:
+       SECRET_KEY = os.environ.get('SECRET_KEY')
+    else:
+       raise ValueError("No SECRET_KEY set please follow the README")
     
     project_dir = os.path.dirname(os.path.abspath(__file__))
     DEBUG = True
