2698e83f41d60bbafafc27397e800f9a28ee879f
Mick Hilhorst <m.hilhorst@goose-it.nl>
2023-06-06 06:18:52 +0200

    Additional changes to .env upgrade

--- run.py
+++ run.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 from app import factory
 
 app = factory.create_app()
+# Load environment variables from .env
+load_dotenv()
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0' , port='5000')
